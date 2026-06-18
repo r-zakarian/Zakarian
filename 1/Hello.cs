49c33faf02c0e855079d08a80668393f041fddcf
rebecca <rebeccazakarian05@gmail.com>
2026-06-18 11:54:19 -0700

    meet u

--- 1/Hello.cs
+++ 1/Hello.cs
@@ -10,3 +10,4 @@ Console.WriteLine($"What's your favorite color?");
 var color = Console.ReadLine();
 Console.WriteLine($"{color} is a nice color!");
 // comment number 1
+Console.WriteLine($"Nice to meet you {user}");
